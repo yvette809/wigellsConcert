33b7e1cfe1911433425668d5f52462e289c01b3d
McSwede <danne-e@hotmail.com>
2025-03-05 19:50:53 +0100

    Remove example code and rename HelloApplication to Main

---
+++ src/main/java/com/example/wiggelsconcert/Main.java
@@ -0,0 +1,17 @@
+package com.example.wiggelsconcert;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
