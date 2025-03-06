28e2e4cf54878e739e91c17fa8cd58b43dfb437b
Jontehurso <jontehurso@gmail.com>
2025-03-06 15:15:19 +0100

    uppdatera

---
+++ src/main/java/com/example/wiggelsconcert/Entities/Concert.java
@@ -0,0 +1,85 @@
+package com.example.wiggelsconcert.Entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+
+@Entity
+public class Concert {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int concert_id;
+
+    private String artist;
+
+    private LocalDate date;
+
+    private double ticket_price;
+
+    private int age_limit;
+
+    @OneToOne
+    @JoinColumn(name = "arena_id")
+    private Arena arena;
+
+    public Concert() {
+
+    }
+
+    public Concert(String artist, LocalDate date, double ticket_price, int age_limit) {
+        this.artist = artist;
+        this.date = date;
+        this.ticket_price = ticket_price;
+        this.age_limit = age_limit;
+    }
+
+    public int getConcert_id() {
+        return concert_id;
+    }
+
+    public void setConcert_id(int concert_id) {
+        this.concert_id = concert_id;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public double getTicket_price() {
+        return ticket_price;
+    }
+
+    public void setTicket_price(double ticket_price) {
+        this.ticket_price = ticket_price;
+    }
+
+    public int getAge_limit() {
+        return age_limit;
+    }
+
+    public void setAge_limit(int age_limit) {
+        this.age_limit = age_limit;
+    }
+
+    public Arena getArena() {
+        return arena;
+    }
+
+    public void setArena(Arena arena) {
+        this.arena = arena;
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.wiggelsconcert {
     requires javafx.controls;
     requires javafx.fxml;
+    requires jakarta.persistence;
 
 
     opens com.example.wiggelsconcert to javafx.fxml;
