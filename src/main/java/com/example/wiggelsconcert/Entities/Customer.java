87ebb5c306db62f4b27ca91986f00c6f924d1dbf
Jontehurso <jontehurso@gmail.com>
2025-03-07 15:09:44 +0100

    uppdatera

--- src/main/java/com/example/wiggelsconcert/Entities/Customer.java
+++ src/main/java/com/example/wiggelsconcert/Entities/Customer.java
@@ -15,7 +15,7 @@ public class Customer {
     private String phone_number;
 
     @OneToOne (cascade = CascadeType.ALL)
-    @JoinColumn(name = "adress_id")
+    @JoinColumn(name = "address_id")
     private Address address;
 
     public Customer() {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,11 @@ module com.example.wiggelsconcert {
     requires javafx.controls;
     requires javafx.fxml;
     requires jakarta.persistence;
+    requires org.hibernate.orm.core;
+    requires java.naming;
 
 
     opens com.example.wiggelsconcert to javafx.fxml;
+    opens com.example.wiggelsconcert.Entities to org.hibernate.orm.core;
     exports com.example.wiggelsconcert;
 }
